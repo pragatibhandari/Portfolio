49e5cc44376b502b1924513a77e6a395c5744917
pragatibhandari <bhandariprag7@gmail.com>
2020-08-27 16:33:16 +0300

    All the file and folders uploaded to github

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function sendContact(){
+    alert("Message cannot be sent, Send message in the email address \: 'bhandariprag7@gmail.com' instead. Thank You.")
+  }
