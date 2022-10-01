788de7f5432d893587e97f87c7674a2682019869
pragatibhandari <bhandariprag7@gmail.com>
2022-10-01 07:45:28 +0300

    new CV added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  function sendContact(){
-    alert("Message cannot be sent, Send message in the email address \: 'bhandariprag7@gmail.com' instead. Thank You.")
-  }
+function sendContact() {
+  alert(
+    "Message cannot be sent, Send message in the email address : 'bhandariprag9@gmail.com' instead. Thank You."
+  );
+}
